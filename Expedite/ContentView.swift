4735c882c274db8bfc1eab7f3d78121debb654ad
Nizar Elfennani <elfennani2002@gmail.com>
2025-07-11 19:57:31 +0100

    First init

--- Expedite/ContentView.swift
+++ Expedite/ContentView.swift
@@ -7,53 +7,32 @@
 
 import SwiftUI
 import SwiftData
+import AsyncDNSResolver
+import Foundation
+import dnssd
+import CFNetwork
+import CAsyncDNSResolver
+import sys_select
+import Network
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        Text("Hello World!")
+            .onAppear {
+                scanNearbyDevices()
             }
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-            .toolbar {
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+    
+    private func scanNearbyDevices(){
+        let browser = NWBrowser(for: .bonjourWithTXTRecord(type: "_expedite._tcp", domain: "local"), using: .tcp)
+        browser.start(queue: .main)
+        
+        browser.browseResultsChangedHandler = { results, _ in
+            print(results)
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
